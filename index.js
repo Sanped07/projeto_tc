546e141acb422b2efb27e0290b29c42f79399030
D3V S4NP3D <118002942+Sanped07@users.noreply.github.com>
2023-06-29 11:31:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function cadastrar(){
     msgError.innerHTML = ''
     
     setTimeout(()=>{
-        window.location.href = 'https://http://127.0.0.1:5500/index.html'
+        window.location.href = 'https://www.louvre.fr/en'
     }, 3000)
   
     
